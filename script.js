d3caa198e7218169820474c002a4a605c80e5276
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-11 16:28:27 +0200

    method defined

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
-      }
-    }
+       emails:[],
+       apiUrl:"https://flynn.boolean.careers/exercises/api/random/mail",
+      };
+    },
+    methods:{
+        getRandomEmails(){
+            this.emails = [];
+            for(let i=0; i<10 ; i++){
+                axios.get(this.apiUrl)
+                .then((response) => {
+                    this.emails.push(response.data.response);
+                });
+            }
+        }
+    },
+    mounted(){
+        this.getRandomEmails();
+    },
   }).mount('#app')
